6ba78f4114babaa5ca639ed7f82b775b75ad9c45
mrbrdo <mrbrdo@gmail.com>
2022-04-17 02:12:59 +0200

    Allow passing custom styles to lazy_image

--- app/helpers/spree/frontend_helper.rb
+++ app/helpers/spree/frontend_helper.rb
@@ -188,11 +188,11 @@ module Spree
       )
     end
 
-    def lazy_image(src:, alt:, width:, height:, srcset: '', **options)
+    def lazy_image(src:, alt:, width:, height:, srcset: '', style: nil, **options)
       # We need placeholder image with the correct size to prevent page from jumping
       placeholder = "data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%20#{width}%20#{height}'%3E%3C/svg%3E"
 
-      image_tag placeholder, data: { src: src, srcset: srcset }, class: "#{options[:class]} lazyload", alt: alt
+      image_tag placeholder, data: { src: src, srcset: srcset }, class: "#{options[:class]} lazyload", alt: alt, style: style
     end
 
     def permitted_product_params
